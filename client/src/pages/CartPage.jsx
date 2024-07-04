b1b92197118b7c56b12ca1087c183b5146372376
emmanuel4365 <128937350+emmanuel4365@users.noreply.github.com>
2024-07-04 12:02:38 +0100

    set function implemented

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -4,6 +4,10 @@ const CartPage = () => {
   let items = useCartState();
   console.log(items);
 
+  const setObj = new Set(items.map(JSON.stringify));
+  const uniqueArray = Array.from(setObj).map(JSON.parse);
+  console.log(uniqueArray);
+
   return (
     <main className="cart-page">
       <div className="cart-page-center">cartPage</div>
